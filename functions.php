7599d2cdfffae0213721c1bc8caf95c06c620f52
k3x4 <xkexagias@gmail.com>
2019-08-07 14:53:39 +0300

    gallery changes

--- functions.php
+++ functions.php
@@ -16,80 +16,22 @@ add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
 
 add_action( 'wp_enqueue_scripts', function(){
     wp_enqueue_script( 'child-theme-front', get_stylesheet_directory_uri() . '/js/front.js', ['jquery'], false, true );
-    wp_enqueue_script( 'child-theme-woo', get_stylesheet_directory_uri() . '/js/woo.js', ['jquery', 'wc-single-product'], false, true );
-    wp_enqueue_style( 'child-theme-flex-css', get_stylesheet_directory_uri() . '/css/flexslider.min.css');
+    // wp_enqueue_script( 'child-theme-woo', get_stylesheet_directory_uri() . '/js/woo.js', ['jquery', 'wc-single-product'], false, true );
+    // wp_enqueue_style( 'child-theme-flex-css', get_stylesheet_directory_uri() . '/css/flexslider.min.css');
 } );
 
-add_filter( 'woocommerce_single_product_carousel_options', function($options){
-    $options['controlNav'] = false;
-    // $options['directionNav'] = true;
-    $options['sync'] = '.k3x4_gallery_thumbs';
+// add_filter( 'woocommerce_single_product_carousel_options', function($options){
+//     // $options['controlNav'] = false;
+//     // $options['directionNav'] = true;
+//     $options['sync'] = '.k3x4_gallery_thumbs';
 
-    return $options;
-} );
+//     return $options;
+// } );
 
-// add_action('init', function(){
-//     remove_action( 'woocommerce_product_thumbnails', 'woocommerce_show_product_thumbnails', 20 );
-// });
+// require_once(dirname(__FILE__) . '/inc/woocommerce-flexslider.php');
+// add_filter( 'woocommerce_single_product_flexslider_enabled', '__return_false' );
 
-// add_filter( 'woocommerce_single_product_image_thumbnail_html', function($html, $post_thumbnail_id){
-//     return k3x4_wc_get_gallery_image_html($post_thumbnail_id, true);
-// }, 10, 2);
-
-// add_action( 'woocommerce_before_single_product_summary', function(){
-add_action( 'woocommerce_product_thumbnails', function(){
-    global $product;
-
-    $attachment_ids = $product->get_gallery_image_ids();
-
-    if ( $attachment_ids && $product->get_image_id() ) {
-        echo '</figure>';
-        echo '<div class="k3x4_gallery_thumbs flexslider">';
-        echo '<ul class="slides">';
-        foreach ( $attachment_ids as $attachment_id ) {
-            echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', k3x4_wc_get_gallery_thumbs_html( $attachment_id ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
-        }
-        echo '</ul>';
-        echo '</div>';
-        echo '<figure>';
-    }
-}, 20 );
-
-function k3x4_wc_get_gallery_image_html( $attachment_id, $main_image = false ) {
-	$flexslider        = (bool) apply_filters( 'woocommerce_single_product_flexslider_enabled', get_theme_support( 'wc-product-gallery-slider' ) );
-	$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
-	$thumbnail_size    = apply_filters( 'woocommerce_gallery_thumbnail_size', array( $gallery_thumbnail['width'], $gallery_thumbnail['height'] ) );
-	$image_size        = apply_filters( 'woocommerce_gallery_image_size', $flexslider || $main_image ? 'woocommerce_single' : $thumbnail_size );
-	$full_size         = apply_filters( 'woocommerce_gallery_full_size', apply_filters( 'woocommerce_product_thumbnails_large_size', 'full' ) );
-	$full_src          = wp_get_attachment_image_src( $attachment_id, $full_size );
-	$image             = wp_get_attachment_image(
-		$attachment_id,
-		$image_size,
-		false,
-		apply_filters(
-			'woocommerce_gallery_image_html_attachment_image_params',
-			array(
-				'title'                   => _wp_specialchars( get_post_field( 'post_title', $attachment_id ), ENT_QUOTES, 'UTF-8', true ),
-				'data-caption'            => _wp_specialchars( get_post_field( 'post_excerpt', $attachment_id ), ENT_QUOTES, 'UTF-8', true ),
-				'data-src'                => esc_url( $full_src[0] ),
-				'data-large_image'        => esc_url( $full_src[0] ),
-				'data-large_image_width'  => esc_attr( $full_src[1] ),
-				'data-large_image_height' => esc_attr( $full_src[2] ),
-				'class'                   => esc_attr( $main_image ? 'wp-post-image' : '' ),
-			),
-			$attachment_id,
-			$image_size,
-			$main_image
-		)
-	);
-
-	return '<div>' . $image . '</div>';
-}
-
-function k3x4_wc_get_gallery_thumbs_html( $attachment_id, $main_image = false ) {
-	$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
-	$thumbnail_size    = apply_filters( 'woocommerce_gallery_thumbnail_size', array( $gallery_thumbnail['width'], $gallery_thumbnail['height'] ) );
-	$thumbnail_src     = wp_get_attachment_image_src( $attachment_id, $thumbnail_size );
-
-	return '<li><img src="' . esc_url( $thumbnail_src[0] ) . '" /></li>';
-}
+// add_filter( 'woocommerce_single_product_carousel_options', function($options){
+//     $options['directionNav'] = true;
+//     return $options;
+// } );
--- js/woo.js
+++ js/woo.js
@@ -1,3 +1,4 @@
+/*
 (function($) {
 
     $('.k3x4_gallery_thumbs').flexslider({
@@ -7,9 +8,116 @@
         slideshow: false,
         itemWidth: 100,
         itemMargin: 5,
-        /*asNavFor: '.woocommerce-product-gallery'*/
-        asNavFor: '.woocommerce-product-gallery__wrapper'
+        asNavFor: '.woocommerce-product-gallery'
     });
 	
 })( jQuery );
+*/
+(function($) {
 
+    var thumbsExist = setInterval(function() {
+        var $elem = $('.flex-control-thumbs');
+        if ($elem.length) {
+            clearInterval(thumbsExist);
+
+            $elem.insertAfter('.woocommerce-product-gallery');
+            $elem.removeClass().addClass('slides');
+            $elem.wrap( "<div class='k3x4_gallery_thumbs flexslider'></div>" );
+            $elem.flexslider({
+                /*
+                animation: "slide",
+                controlNav: false,
+                animationLoop: true,
+                slideshow: false,
+                itemWidth: 100,
+                itemMargin: 24,
+                maxItems: 4,
+                minItems: 3,
+                */
+               controlNav: false,
+                animationLoop: false,
+                slideshow: false,
+                itemWidth: 210,
+                itemMargin: 5,
+                asNavFor: '.woocommerce-product-gallery'
+            });
+
+            $('.woocommerce-product-gallery').addClass('flexslider').flexslider({
+                animation: "slide",
+                controlNav: false,
+                animationLoop: false,
+                slideshow: false,
+                sync: ".k3x4_gallery_thumbs"
+            });
+        }
+    }, 500);
+
+    /*
+    setTimeout(function () {
+        var thubmNav = $('.flex-control-thumbs');
+            thubmNav.addClass('slides');
+            $('.flex-control-thumbs').flexslider({
+                animation: "slide",
+                controlNav: false,
+                animationLoop: true,
+                slideshow: false,
+                itemWidth: 100,
+                itemMargin: 24,
+                maxItems: 4,
+                minItems: 3,
+            });
+    }, 100);
+    */
+
+/****************Variation product js code ****************************************/
+
+$(".variations_form").on("wc_additional_variation_images_frontend_ajax_response_callback", function (event, variation) {
+    console.log("variation");
+    setTimeout(function () {
+        var thubmNav = $(".woocommerce - product - gallery.flex - control - nav");
+        if (thubmNav.length) {
+            /*if(!thubmNav.parent().hasClass("navWrapper")){
+            thubmNav.wrap("
+            
+            ");
+            var itemsLength_ = thubmNav.children("li").length
+            var itemWidth_ = thubmNav.children("li").outerWidth();
+            thubmNav.width( itemWidth_* itemsLength_);
+            thubmNav.parent().css({"width": $(".woocommerce-product-gallery").width(), "overflow":"hidden"});
+            
+            var navigationDiv_ = $("
+            
+            ", {class: "navigationWrapper"});
+            var leftNavigation_ = $("", {class: "leftNavigation"});
+            var rightNavigation_ = $("", {class: "rightNavigation"});
+            navigationDiv_.append(leftNavigation_).append(rightNavigation_);
+            navigationDiv_.insertAfter(thubmNav);
+            leftNavigation_.on("click", function(){
+            
+            });
+            rightNavigation_.on("click", function(){
+            
+            });
+            }*/
+            //console.log("asd", !thubmNav.closest(".navWrapper").length)
+            if (!thubmNav.closest(".navWrapper").length) {
+                thubmNav.addClass("slides");
+                thubmNav.wrap("");
+                $('.navWrapper').flexslider({
+                    animation: "slide",
+                    controlNav: false,
+                    animationLoop: false,
+                    slideshow: false,
+                    itemWidth: 100,
+                    itemMargin: 24,
+                    maxItems: 4,
+                    minItems: 3,
+                });
+            }
+
+        }
+    }, 100);
+});
+
+
+})( jQuery );
